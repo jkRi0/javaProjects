dea7ba61d64995173547ee3e16b75475f34b5366
jkRi0 <rioverosjustine92@gmail.com>
2025-02-04 21:21:01 +0800

    Add files via upload

---
+++ 2ITC_CCS108/src/finals/Abstraction.java
@@ -0,0 +1,24 @@
+package finals;
+
+abstract class X{
+	public abstract void sample();
+}
+
+interface Y{
+	public String asd(String bruh);
+}
+
+class Z extends X{
+	public void sample() {
+		System.out.println("hii");
+	}
+}
+
+
+public class Abstraction {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
